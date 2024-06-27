dd8d88158bfa8ac0f9aceb13af6d2e8a33f514bf
Aamirah <aamirahpatel@hotmail.co.uk>
2024-06-27 04:15:26 +0100

    doing a bad thing

--- API/my-library/index.ts
+++ API/my-library/index.ts
@@ -1,11 +1,11 @@
 import mongoose from "mongoose";
 import app from "./app";
-import 'dotenv/config';
+import "dotenv/config";
 
-console.log('Loaded MONGODB_URI:', process.env.MONGODB_URI);  
 const PORT = 8080;
 
-const DB_URI = process.env.MONGODB_URI;
+const DB_URI =
+  "mongodb+srv://mongo_db_service_user:5xDpmCoC0rsDceXf@cluster0.knsb4bc.mongodb.net/my-library?retryWrites=true&w=majority&appName=Cluster0";
 
 if (!DB_URI) {
   throw new Error("MONGODB_URI environment variable is not defined");
